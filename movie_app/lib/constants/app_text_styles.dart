c846a29c1d3956b09313e6c439ddc24a62393859
Levanttt <luthfiardi66@gmail.com>
2026-07-04 17:29:45 +0700

    Define Colors, Textstyles, and install dependencies

---
+++ movie_app/lib/constants/app_text_styles.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTextStyles {
+  // Heading
+  static TextStyle get displayLarge => GoogleFonts.poppins(
+    fontSize: 28,
+    fontWeight: FontWeight.w700,
+    height: 1.2,
+  );
+
+  static TextStyle get headingLarge => GoogleFonts.poppins(
+    fontSize: 22,
+    fontWeight: FontWeight.w700,
+    height: 1.3,
+  );
+
+  static TextStyle get headingMedium => GoogleFonts.poppins(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    height: 1.3,
+  );
+
+  static TextStyle get headingSmall => GoogleFonts.poppins(
+    fontSize: 15,
+    fontWeight: FontWeight.w600,
+    height: 1.3,
+  );
+
+  // Body
+  static TextStyle get bodyLarge => GoogleFonts.inter(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    height: 1.5,
+  );
+
+  static TextStyle get bodyMedium => GoogleFonts.inter(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    height: 1.5,
+  );
+
+  static TextStyle get bodySmall => GoogleFonts.inter(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    height: 1.4,
+  );
+
+  static TextStyle get caption => GoogleFonts.inter(
+    fontSize: 11,
+    fontWeight: FontWeight.w500,
+    height: 1.3,
+  );
+
+  // Button
+  static TextStyle get button => GoogleFonts.poppins(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    height: 1.2,
+  );
+}
