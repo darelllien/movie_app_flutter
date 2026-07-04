8f6470194ea4b8dfde3830869719074c157bfdfd
Levanttt <luthfiardi66@gmail.com>
2026-07-04 20:01:16 +0700

    Add Header Section

--- movie_app/lib/screens/main/home_tab.dart
+++ movie_app/lib/screens/main/home_tab.dart
@@ -1,14 +1,92 @@
 import 'package:flutter/material.dart';
+import '../../constants/app_color.dart';
+import '../../constants/app_text_styles.dart';
 
 class HomeTab extends StatelessWidget {
   const HomeTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Ini Halaman Beranda', style: TextStyle(fontSize: 20)),
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _buildHeader(),
+
+                const SizedBox(height: 24),
+
+              ],
+            ),
+          ),
+        ),
       ),
+    );
+  }
+
+  Widget _buildHeader() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Halo, Irfan! 👋',
+                  style: AppTextStyles.bodyMedium.copyWith(
+                    color: AppColors.textSecondary,
+                  ),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  'Mau nonton apa hari ini?',
+                  style: AppTextStyles.headingMedium.copyWith(
+                    color: AppColors.textPrimary,
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                color: AppColors.surface,
+                shape: BoxShape.circle,
+              ),
+              child: IconButton(
+                icon: const Icon(Icons.notifications_none),
+                color: AppColors.textPrimary,
+                onPressed: () {
+                },
+              ),
+            ),
+          ],
+        ),
+
+        const SizedBox(height: 20),
+
+        TextField(
+          decoration: InputDecoration(
+            hintText: 'Cari judul film atau bioskop...',
+            hintStyle: AppTextStyles.bodyMedium.copyWith(
+              color: AppColors.textSecondary,
+            ),
+            prefixIcon: const Icon(Icons.search, color: AppColors.textSecondary),
+            filled: true,
+            fillColor: AppColors.surface,
+            contentPadding: const EdgeInsets.symmetric(vertical: 14),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: BorderSide.none,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
